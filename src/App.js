e5bef0826ba4fc45c40151a71279868994ce1d78
Bak David <bak.david96@gmail.com>
2021-10-31 11:34:06 +0200

    Npm firebase related dependencies and setup firebase connection

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBgur3yxdtIne5ekjNPppRO-_lvcKEPFOk",
+  authDomain: "fir-chat-app-466b2.firebaseapp.com",
+  projectId: "fir-chat-app-466b2",
+  storageBucket: "fir-chat-app-466b2.appspot.com",
+  messagingSenderId: "201456430988",
+  appId: "1:201456430988:web:57495f4947c65b306bb0a9",
+  measurementId: "G-JGJS78MQYR"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
